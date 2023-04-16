324c16615c61177c0ecc375e32c9d544bf4deeaf
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-04-16 18:30:05 -0300

    CustomFooter: Footer do projeto

---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="font-bold grid lg:grid-cols-3 px-4 lg:px-10 gap-4 items-center py-3 w-full h-fit justify-items-center bg-white shadow-inner"
+  >
+    <span class="col-start-2 font-[300] gap-x-1 flex items-center">
+      Developed by
+      <a
+        :href="getSocialMediaByLabel('Linkedin')?.url"
+        target="_blank"
+        class="font-extrabold"
+        >@oituty</a
+      >
+    </span>
+
+    <div class="flex lg:justify-self-end">
+      <ul class="flex gap-x-2.5">
+        <li
+          v-for="socialMedia in socialMedias"
+          :key="socialMedia.label"
+          class="cursor-pointer rounded-sm border-secondary shadow-inner h-9 w-9 border-[0.063rem]"
+        >
+          <a
+            :href="socialMedia.url"
+            class="h-full w-full flex justify-center items-center"
+            target="_blank"
+          >
+            <img
+              :src="getIcon(socialMedia.iconPath)"
+              :alt="`Ícone ${socialMedia.label}`"
+            />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getIcon, socialMedias } from "../utils";
+
+function getSocialMediaByLabel(label: "Instagram" | "Linkedin") {
+  return socialMedias.find((media) => media.label === label);
+}
+</script>
