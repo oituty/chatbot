7b4eef6bd02bfb86e0a21b75a8a62fa14ff0a896
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-04-16 18:29:53 -0300

    CustomButton: Botão com variações de estilo

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button type="button" :class="[styleBase, buttonVariants[variant]]">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    variant: "elevated" | "outlined";
+  }>(),
+  {
+    variant: "elevated",
+  }
+);
+
+const styleBase =
+  "rounded-sm max-w-[14rem] border-2 px-6 py-2.5 w-full transition-all";
+const buttonVariants: { elevated: string; outlined: string } = {
+  elevated:
+    "bg-button-primary text-white border-primary hover:border-secondary hover:bg-secondary ",
+  outlined: "bg-transparent hover:text-secondary text-primary border-primary",
+};
+</script>
