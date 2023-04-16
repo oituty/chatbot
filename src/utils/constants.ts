487a1e0463f762a7be785ced69a28141972e699f
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-04-16 18:31:38 -0300

    utils/constants: Variaveis uteis

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+export const socialMedias: { label: string; url: string; iconPath: string }[] =
+  [
+    {
+      label: "Linkedin",
+      url: "https://www.linkedin.com/in/oituty/",
+      iconPath: "linkedin.svg",
+    },
+    {
+      label: "Instagram",
+      url: "https://www.instagram.com/oituty/",
+      iconPath: "instagram.svg",
+    },
+  ];
