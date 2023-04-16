8095b1cec0bcc3fcb6a47e0de1b2d0a55e8742e6
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-04-16 18:27:19 -0300

    eslintrc.js: regras de formatação

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,38 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    "vue/setup-compiler-macros": true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser",
   },
+  plugins: ["@typescript-eslint", "eslint-plugin-import-helpers"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
   rules: {
+    "vue/multi-word-component-names": "off",
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "always", prev: "*", next: "function" },
+      { blankLine: "always", prev: "function", next: "*" },
+      { blankLine: "always", prev: "import", next: "*" },
+      { blankLine: "any", prev: "import", next: "import" },
+    ],
+    curly: "error",
+    "import-helpers/order-imports": [
+      "error",
+      {
+        newlinesBetween: "always",
+        groups: ["module", "parent", ["sibling", "index"]],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
