9a36923e858c0f6c85aef3be5942fc10ebd5d04c
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-04-17 11:18:29 -0300

    ChatFooter: Botão de envio e input de mensagem do chat

---
+++ src/components/ChatFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="py-2.5 w-full gap-x-4 px-6 flex justify-between">
+    <input
+      id="messageInput"
+      :class="[
+        'w-full bg-tertiary rounded-sm px-4 resize-none border-transparent overflow-hidden border-2 outline-none transition-all focus:outline-none',
+        { 'border-2 !border-primary': isFocused },
+      ]"
+      placeholder="What do you want to know?"
+      type="text"
+      :value="message"
+      @blur="isFocused = false"
+      @focus="isFocused = true"
+    />
+    <CustomButton class="px-6 w-fit">Send</CustomButton>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+import CustomButton from "./CustomButton.vue";
+
+const message = ref("");
+const isFocused = ref(false);
+</script>
