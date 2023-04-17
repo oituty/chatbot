bddfc361c0a8b97164b41a2877096999d3d57e45
tutyrodrigues <34607596+tutyrodrigues@users.noreply.github.com>
2023-04-17 11:15:43 -0300

    Constants: Adicionada nova media social

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,4 +10,9 @@ export const socialMedias: { label: string; url: string; iconPath: string }[] =
       url: "https://www.instagram.com/oituty/",
       iconPath: "instagram.svg",
     },
+    {
+      label: "Email",
+      url: "mailto:taynaalro@gmail.com?subject=Contact - Chatbot&body=I accessed your website and would like to talk more, please reply to this email so we can set up a meeting",
+      iconPath: "email.svg",
+    },
   ];
